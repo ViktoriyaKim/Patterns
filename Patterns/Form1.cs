08489324642181928b183f6b0900f7af04a69fd8
Viktoriya Kim <vikakim083@gmail.com>
2021-12-08 22:32:41 +0300

    Добавьте файлы проекта.

---
+++ Patterns/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Patterns
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Delegating delegateForm = new Delegating();
+
+            delegateForm.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            AbstractSuperclassForm superclassForm = new AbstractSuperclassForm();
+
+            superclassForm.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FactoryMethodForm factoryMethodForm = new FactoryMethodForm();
+
+            factoryMethodForm.Show();
+        }
+    }
+}
